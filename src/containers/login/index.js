2bd4eb093ce6eb659ba7eda308fb4010cb3fb71a
shanshuirenjia <978987373@qq.com>
2020-03-25 09:23:31 +0800

    init develop env

---
+++ src/containers/login/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+
+};
+
+class Login extends React.Component {
+
+  render() {
+    return (
+      <div>登录界面</div>
+    );
+  }
+}
+
+Login.propTypes = propTypes;
+
+export default Login;
